16564dbfa8bb122aebcc4ca45dd39da2e3c28743
ToTheMoons-ER <641463007@crru.ac.th>
2024-02-09 00:16:04 +0700

    command -a

---
+++ exam-1/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Got Username and Password from keyboard
+	var username string
+	var password string
+
+	fmt.Print("Username: ")
+	fmt.Scanln(&username)
+	fmt.Print("Password: ")
+	fmt.Scanln(&password)
+	// Call function LogIn
+
+	LogIn(username, password)
+
+	fmt.Println(Add(2, 3))
+	fmt.Println(Subtract(5, 3))
+
+}
+
+func HelloFunction() string {
+	return "Hello"
+}
+func Add(a int, b int) int {
+	return a + b
+}
+
+func Subtract(a, b int) int {
+	return a - b
+}
+
+func UserLogin(username string, password string) bool {
+	if username == "admin" && password == "password" {
+		return true
+	} else {
+		return false
+	}
+}
+
+// Use function UserLogin
+func LogIn(username string, password string) {
+	if UserLogin(username, password) {
+		fmt.Println("Login successful")
+	} else {
+		fmt.Println("Login failed")
+	}
+}
